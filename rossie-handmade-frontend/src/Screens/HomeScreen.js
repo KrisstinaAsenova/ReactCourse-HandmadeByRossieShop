18bfc3a5e60eb4f6daaa4450cdfa03ace2c869ad
Kristina Asenova <krisstina.asenova@gmail.com>
2021-06-29 00:55:47 +0300

    Redux-manage state

--- rossie-handmade-frontend/src/Screens/HomeScreen.js
+++ rossie-handmade-frontend/src/Screens/HomeScreen.js
@@ -1,22 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import {Link} from 'react-router-dom';
-import axios from 'axios';
+import { useSelector, useDispatch } from 'react-redux';
+import { listProducts } from '../actions/productActions';
 
 function HomeScreen(props) {
 
-    const [products, setProduct] = useState([]);
-    useEffect(() => {
-        const fetchData = async () => {
-            const {data} = await axios.get("/api/products");
-            setProduct(data);
-        }
-        fetchData();
+    const productList = useSelector(state => state.productList);
+    const { products, loading, error } = productList;
+    const dispatch = useDispatch();
+
+    useEffect(() => {    
+        dispatch(listProducts());
+
         return () => {
             
         };
     }, []);
 
-    return  <ul className="products">
+    return loading? <div>Loading...</div> :
+    error? <div>{error}</div> :
+    <ul className="products">
     {
        products.map(product =>
         <li key={product.id}>
