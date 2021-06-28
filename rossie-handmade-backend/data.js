f7b1d7a0140d6792302e7d7f74ae2d48ec6c5d9f
Kristina Asenova <krisstina.asenova@gmail.com>
2021-06-28 23:24:10 +0300

    Added server

---
+++ rossie-handmade-backend/data.js
@@ -0,0 +1,44 @@
+export default {
+    products:[
+    {
+        id:'1',
+        name: 'Cake',
+        image: '/images/kinder.jpg',
+        category: 'Cakes',
+        price:60,
+        brand: 'Rossie',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        id:'2',
+        name: 'Book mark',
+        image: '/images/kinder.jpg',
+        category: 'Marks',
+        price:10,
+        brand: 'Rossie',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        id:'3',
+        name: 'Cup',
+        image: '/images/kinder.jpg',
+        category: 'Polymer clay',
+        price:30,
+        brand: 'Rossie',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        id:'4',
+        name: 'Knitted bunny',
+        image: '/images/kinder.jpg',
+        category: 'Knitted',
+        price:25,
+        brand: 'Rossie',
+        rating: 4.5,
+        numReviews: 10
+    },
+]
+}
