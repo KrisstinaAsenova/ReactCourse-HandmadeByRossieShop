f84cc060151dd578d214d99a4da343802741cd65
Kristina Asenova <krisstina.asenova@gmail.com>
2021-07-01 07:51:15 +0300

    Fixed user cookie

--- rossie-handmade-frontend/src/store.js
+++ rossie-handmade-frontend/src/store.js
@@ -7,8 +7,12 @@ import Cookie from 'js-cookie';
 import { userRegisterReducer, userSigninReducer } from './reducers/userReducers';
 
 const cartItems = Cookie.getJSON("cartItems") || [];
+const userInfo = Cookie.getJSON('userInfo') || null;
 
-const initialState = {cart: {cartItems}};
+const initialState = {
+    cart: {cartItems},
+    userSignin: { userInfo }};
+    
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
