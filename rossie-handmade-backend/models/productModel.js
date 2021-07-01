181d2eaa47b2bc5e3e4c4d5c6ed2ee43b4e5a45c
Kristina Asenova <krisstina.asenova@gmail.com>
2021-07-01 18:55:21 +0300

    Adding user profile and shopping screens

--- rossie-handmade-backend/models/productModel.js
+++ rossie-handmade-backend/models/productModel.js
@@ -3,12 +3,12 @@ import mongoose from 'mongoose';
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true, minLength: 3},
   image: { type: String, required: true },
-  brand: { type: String, required: true },
+  brand: { type: String, required: true, minLength: 3 },
   category: { type: String, required: true },  
   countInStock: { type: String, default: 0, required: true },
   price: { type: Number, default: 0, required: true },
-  qty: { type: Number, default: 10 },
-  description: { type: String, required: true },
+  countInStock: { type: Number, default: 0, required:true },
+  description: { type: String, required: true, minLength: 3 },
   rating: { type: Number, default: 0, required: true },
   numReviews: { type: Number, default: 0, required: true }
 });
